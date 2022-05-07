1053d4e915ea0ece2c5598c16b7167225505424f
DanLen97 <dan.lengauer@gmail.com>
2022-05-07 17:38:16 +0200

    Fix tests

--- as-engine/tests/index.js
+++ as-engine/tests/index.js
@@ -1,9 +1,9 @@
 import assert from "assert";
-import { instantiate } from "../build/release.js";
+import { instantiate } from "../dist/release.js";
 import { readFile } from "fs/promises";
 
 (async () => {
-  const wasmArrayBuffer = await readFile("./build/release.wasm");
+  const wasmArrayBuffer = await readFile("./dist/release.wasm");
   const module = await WebAssembly.compile(wasmArrayBuffer);
   const wasm = await instantiate(module, { env: { memory: new WebAssembly.Memory({ initial: 256 }) } });
 
